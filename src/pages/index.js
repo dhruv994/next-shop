6e37d113abe1c1c043caf1b12188e6b06af94095
Dhruv <dhruv.sharma994@gmail.com>
2023-10-05 17:32:36 -0400

    added code to fetch data from cms(content managment server)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,39 @@
 
 import Head from 'next/head'
 import Title from '../../components/title'
+import { getProducts } from '../../lib/products'
 
 
-export default function HomePage() {
+
+export async function getStaticProps() {
+  console.log("Home Page [get static props]")
+
+  const products = await getProducts();
+  return {
+    props: { products }
+  }
+}
+
+
+export default function HomePage({ products }) {
+  console.log("Products in homepage",products);
   return (
     <>
-    <Head>
-      <title>
-        Next Shop
-      </title>
-    </Head>
-    <main className='p-2'>
-      <Title > Next Shop</Title>
-    </main>
+      <Head>
+        <title>
+          Next Shop
+        </title>
+      </Head>
+      <main className='p-2'>
+        <Title > Next Shop</Title>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          ))}
+        </ul>
+      </main>
     </>
-    )
+  )
 }
