15f7258c727abe94a170975892850dc2695fae64
Dhruv <dhruv.sharma994@gmail.com>
2023-10-23 14:19:56 -0400

    added some styles

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -4,7 +4,7 @@ export default function ProductCard({ product }) {
     return (
         <div className="border my-4 w-80 shadow hover:shadow-xl">
             <Link href={`products/${product.id}`}>
-                <img src="https://dummyimage.com/320x240" alt="" />
+                <img src={product.pictureUrl} alt="" />
                 <div className="p-2 flex justify-between items-baseline">
                     <h2 className="text-lg font-bold">
                         {product.title}
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function HomePage({ products }) {
       </Head>
       <main className='p-2'>
         <Title > Next Shop</Title>
-        <ul className='grid grid-cols-3'>
+        <ul className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
           {products.map((product) => (
             <li key={product.id}>
               <ProductCard product={product} />
